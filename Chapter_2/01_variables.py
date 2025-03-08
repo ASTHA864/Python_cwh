54f690aab8f42a3e3aaa2adfd9ba70f8062fc34d
Astha Gupta <asthagupta86400@gmail.com>
2025-03-08 23:31:50 +0530

    commit

---
+++ Chapter_2/01_variables.py
@@ -0,0 +1,19 @@
+a=5 #integer
+b=4.2 #float
+c="Python" #string
+d=True  #boolean
+e=None  #NoneType
+print(a+b)
+print(c)
+print(d)
+print(e)
+type_of_a= type(a)
+print(type_of_a)
+type_of_b= type(b)
+print(type_of_b)
+type_of_c= type(c)
+print(type_of_c)
+type_of_d= type(d)
+print(type_of_d)
+type_of_e= type(e)
+print(type_of_e)
---
+++ Chapter_2/02_operators.py
@@ -0,0 +1,40 @@
+
+# Arithmetic Operators
+print("Arithmetic Operators")
+
+a=12
+b=4
+print(a+b) 
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+
+
+# Assignment Operators
+print("Assignment Operators")
+
+a=4-1
+print(a)
+b=3
+b=b+3
+print(b)
+
+# Comparison  Operators
+print("Comparison Operators")
+
+print(3<2)
+print(3>2)
+print(3!=2)
+print(3==3)
+
+
+# Logical Operators
+print("Logical Operators")
+
+print(3<2 and 3>2)
+print(3==3 and 4!=8)
+print(3<2 or 3>2)
+print(3==3 or 4==8)
+print(not 3==3)
