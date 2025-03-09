39b3594392fd9715a110ae4fb9e2d36d3457f613
Astha Gupta <asthagupta86400@gmail.com>
2025-03-09 13:43:00 +0530

    commit

--- Chapter_1/modules.py
+++ Chapter_1/modules.py
@@ -2,3 +2,11 @@ import pyjokes
 
 jokes= pyjokes.get_joke()
 print(jokes)
+
+
+
+# Git Commands:
+# git init
+# git add .
+# git commit -m "commit"
+# git push -u https://github.com/ASTHA864/Python.git
